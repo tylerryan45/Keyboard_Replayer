1ea58f5fe961bc4e5f718e48cd317fbe49f6e953
tylerryan45 <tylerryan2022@gmail.com>
2023-04-03 23:11:36 -0500

    started adding methods to save inputs to a file. Issue with controller not closing correctly needs fixed.

--- Keyboard_Replayer.py
+++ Keyboard_Replayer.py
@@ -7,7 +7,7 @@ end_time: float
 recorded_key_presses: dict = {}
 
 
-def keyboard_record():
+def record_keyboard():
     global start_time
     global end_time
     global keys_pressed
@@ -43,7 +43,7 @@ def keyboard_record():
     keys_pressed = set()
     with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
-        listener.stop()
+    listener.stop()
     return recorded_key_presses
 
 
@@ -89,6 +89,16 @@ def release_special_key(key, controller):
         controller.release(Key.backspace)
 
 
+def wait_for_go():
+    def on_press(key):
+        if key == keyboard.Key.enter:
+            return False
+
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+    listener.stop()
+
+
 def replay(inputs):
     controller = Controller()
     for action in inputs:
@@ -105,19 +115,61 @@ def replay(inputs):
                 release_special_key(action[1][:-2], controller)
 
 
-def wait_for_go():
-    def on_press(key):
-        if key == keyboard.Key.esc or keyboard.Key.enter:
-            return False
+def create_file(file_name):
+    try:
+        file = open(file_name + ".txt", "x")
+        return True
+    except FileExistsError:
+        print(f"{file_name} already exists. Would you like to overwrite it? \n1 = yes\n2 = no")
+        while True:
+            try:
+                response = input()
+                if response == "1":
+                    return True
+                elif response == "2":
+                    return False
+                else:
+                    raise TypeError
+            except TypeError:
+                print(f"enter only 1 or 2. \nWould you like to overwrite {file_name}.txt? \n1 = yes\n2 = no")
 
-    with keyboard.Listener(on_press=on_press) as listener:
-        listener.join()
-        listener.stop()
+
+def user_saves_file():
+    while True:
+        try:
+            print("Would you like to save the recorded inputs? \n1 = yes\n2 = no")
+            result = input()
+            if result == "1":
+                return True
+            elif result == "2":
+                return False
+            else:
+                raise TypeError
+        except TypeError:
+            print(result)
+            print(f"Enter only 1 or 2.")
+
+
+def write_file(recorded_inputs, file_name):
+    file = open(file_name + ".txt", "w")
+    for action in recorded_inputs:
+        file.write(action + "\n")
+
+
+def make_file(recorded_inputs):
+    file_name = input("Enter the name of the file you would like to save the inputs to.")
+    if create_file(file_name):
+        write_file(recorded_inputs, file_name)
+        print("file created")
 
 
 if __name__ == "__main__":
-    recorded_inputs = keyboard_record()
+    recorded_inputs = record_keyboard()
     wait_for_go()
-
     replay(recorded_inputs)
+    print("finished replaying inputs")
+    if user_saves_file():
+        make_file(recorded_inputs)
     print("done")
+
+
